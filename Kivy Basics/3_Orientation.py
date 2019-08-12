fe30a965013cb40c4d983f8237824f7e18bf99bf
AL OUATIQ <alouatiq@outlook.com>
2019-08-12 17:46:17 +0300

    basics

---
+++ Kivy Basics/3_Orientation.py
@@ -0,0 +1,19 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.base import runTouchApp
+Builder.load_string("""
+<BoxLayout>:
+    orientation: 'horizontal'
+    Button:
+        text: 'B1'
+    Button:
+        text: 'B2'
+    Button:
+        text: 'B3'
+
+""")
+class MyListView(BoxLayout):
+    pass
+if __name__== '__main__':
+    runTouchApp(MyListView())
