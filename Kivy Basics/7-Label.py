ff962e3fca3aae00686e616f73fbedf97c990544
AL OUATIQ <alouatiq@outlook.com>
2019-08-12 17:55:04 +0300

    add label

--- Kivy Basics/7-Label.py
+++ Kivy Basics/7-Label.py
@@ -1,2 +1,10 @@
 from kivy.app import App
 from kivy.uix.label import Label
+
+class MyApp(App):
+	"""docstring for ClassName"""
+	def build(self):
+		return Label(text='Hello work')
+
+if __name__ =='__main__':
+	MyApp().run()
